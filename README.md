57738b3158e7d09e8df2d6ef1f7c19ae4f2f351b
Shafayath Jamil Rafi <shafayathj@gmail.com>
2025-01-27 23:37:37 +0600

    README.md added

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# React + Vite
+# Project - Gadget Heaven
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- Live Link: [Gadget Heaven](https://dream-11-rafee.netlify.app/)
 
-Currently, two official plugins are available:
+## Brief Description
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Welcome to Gadget Heaven! This website is your ultimate destination for exploring the latest and greatest in tech gadgets. Whether you're a tech enthusiast looking to browse the newest devices, or a shopper ready to add your favorite items to the cart and wishlist, Gadget Heaven has you covered.
+
+## Features
+
+- **Browse Gadgets:** Discover a wide range of tech gadgets with detailed descriptions and images.
+- **Add to Cart:** Conveniently add items to your shopping cart for a seamless purchasing experience.
+- **Add to Wishlist:** Save your favorite gadgets to your wishlist for future reference.
+
+## Technologies Used
+
+- **React:** A powerful JavaScript library for building user interfaces, enabling a responsive and dynamic user experience.
+- **React Router DOM:** Facilitates seamless navigation across different pages of the website without reloading the entire page.
+- **ES6:** The latest version of ECMAScript, providing modern syntax and features to enhance the development process.
--- src/pages/DisplayCart.jsx
+++ src/pages/DisplayCart.jsx
@@ -1,6 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { AddToCartContext } from "../MainLayout";
 import CartBoxCard from "../components/CartBoxCard";
+import { HiMiniBarsArrowDown } from "react-icons/hi2";
+import paymentIcon from "../assets/Group.png";
+import toast from "react-hot-toast";
 
 const DisplayCart = () => {
   const { addToCart, setAddToCart } = useContext(AddToCartContext);
@@ -11,6 +14,11 @@ const DisplayCart = () => {
       (product) => product.product_id !== itemToRemove.product_id
     );
     setAddToCart(newList);
+  };
+  // Sort By Price
+  const handleDescendingSort = () => {
+    const filtered = [...addToCart].sort((a, b) => b.price - a.price);
+    setAddToCart(filtered);
   };
   // Update Total Price
   useEffect(() => {
@@ -28,10 +36,24 @@ const DisplayCart = () => {
           <h2 className="text-2xl font-bold text-[#0B0B0B]">
             Total Cost: ${totalPrice.toFixed(2)}
           </h2>
-          <button className="border border-[#8332C5] text-base text-[#9538E2] py-2 px-6 rounded-full">
-            Sort By Price
+          <button
+            onClick={handleDescendingSort}
+            className="border border-[#8332C5] text-base text-[#9538E2] py-2 px-6 rounded-full inline-flex items-center gap-2 hover:bg-[#8332C5] hover:text-white cursor-pointer"
+          >
+            <span> Sort By Price</span>
+            <HiMiniBarsArrowDown className="size-5" />
           </button>
-          <button className="bg-[#9538E2] text-base text-white py-2 px-6 rounded-full">
+          <button
+            onClick={() => {
+              if (addToCart.length !== 0) {
+                document.getElementById("paymentModal").showModal();
+                setAddToCart([]);
+              } else {
+                toast.error("Sorry!! Your Cart is Empty");
+              }
+            }}
+            className="bg-[#9538E2] text-base text-white py-2 px-6 rounded-full hover:text-[#8332C5] hover:bg-white cursor-pointer border"
+          >
             Purchase
           </button>
         </div>
@@ -43,6 +65,28 @@ const DisplayCart = () => {
           handleRemoveItem={handleRemoveItem}
         />
       ))}
+
+      {/* Modal Here */}
+      <dialog id="paymentModal" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box p-8 md:px-12">
+          <div className="py-4 text-center mx-auto space-y-5 border-b border-[#09080F1A]">
+            <img
+              src={paymentIcon}
+              alt="Verified Payment Logo"
+              className="mx-auto"
+            />
+            <h2 className="text-2xl font-bold">Payment Successful</h2>
+          </div>
+
+          <div className="py-6 text-center space-y-4">
+            <p className="text-[#09080F99]">Thanks for Purchasing</p>
+            <p className="text-[#09080F99]">Total: ${totalPrice}</p>
+            <form method="dialog">
+              <button className="btn block w-full rounded-full">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
